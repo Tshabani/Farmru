1291391942110a6b43e13ff4c6860531d226485d
Katlego Rambau <emergingtechwithkat@gmail.com>
2024-11-09 22:15:37 +0200

    Fix node data view
    Fix node data view

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, Injector, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Injector, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { AppComponentBase } from '@shared/app-component-base';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
 import { GoogleChartInterface, GoogleChartType } from 'ng2-google-charts';
+import { AppStatisticsDto, HomeServiceProxy } from '@shared/service-proxies/service-proxies';
+import { finalize } from 'rxjs';
 
 @Component({
   templateUrl: './home.component.html',
@@ -9,15 +11,16 @@ import { GoogleChartInterface, GoogleChartType } from 'ng2-google-charts';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomeComponent extends AppComponentBase {
-  //stats: AppStatisticsDto;
+  stats: AppStatisticsDto = new AppStatisticsDto;
+
   public pieChart: GoogleChartInterface = {
     chartType: GoogleChartType.PieChart,
     dataTable: [
       ['System Stats', ''],
       ['Roles', 0],
       ['Users', 0],
-      ['Alerts', 0],
-      ['Logs', 0]
+      ['Nodes', 0],
+      ['ActiveNodes', 0]
     ],
     //firstRowIsData: true,
     options: {'title': 'System stats'},
@@ -26,7 +29,7 @@ export class HomeComponent extends AppComponentBase {
   public barChart: GoogleChartInterface = {
     chartType: GoogleChartType.BarChart,
     dataTable: [
-      ['System Stats', 'Roles', 'Users', 'Alerts', 'Logs'],
+      ['System Stats', 'Roles', 'Users', 'Nodes', 'ActiveNodes'],
       ['total', 0, 0, 0, 0]
     ],
     options: {
@@ -37,20 +40,44 @@ export class HomeComponent extends AppComponentBase {
     }
   };
 
-  constructor(injector: Injector) {
+  constructor(
+    injector: Injector,
+    cd: ChangeDetectorRef,
+    private _homeService: HomeServiceProxy   
+    ) {
     super(injector);
+  }
 
-    this.pieChart.dataTable = [
-      ['System Stats', ''],
-      ['Roles', 5],
-      ['Users', 3],
-      ['Alerts', 7],
-      ['Logs', 10]
-    ];
+ngOnInit(): void {
+  this.list();
+}
 
-    this.barChart.dataTable = [
-      ['System Stats', 'Roles', 'Users', 'Alerts', 'Logs'],
-      ['total', 4, 7, 5, 1]
-    ];
+protected list(
+    //finishedCallback: Function
+): void {
+    this._homeService
+    .getAppStats(
+    )
+    .pipe(
+        finalize(() => {
+          //finishedCallback();
+        })
+    )
+    .subscribe((result) => {
+        this.stats = result;
+        this.pieChart.dataTable = [
+          ['System Stats', ''],
+          ['Roles', result.totalNumberOfRoles],
+          ['Users', result.totalNumberOfUsers],
+          ['Nodes', result.totalNumberOfNodes],
+          ['ActiveNodes', result.totalNumberOfActiveNodes]
+        ];
+
+        this.barChart.dataTable = [
+          ['System Stats', 'Roles', 'Users', 'Nodes', 'ActiveNodes'],
+          ['total', result.totalNumberOfRoles, result.totalNumberOfUsers, result.totalNumberOfNodes, result.totalNumberOfActiveNodes]
+        ];
+    });
   }
 }
+
--- angular/src/shared/service-proxies/service-proxy.module.ts
+++ angular/src/shared/service-proxies/service-proxy.module.ts
@@ -6,7 +6,7 @@ import * as ApiServiceProxies from './service-proxies';
 
 @NgModule({
     providers: [
-        //ApiServiceProxies.HomeServiceProxy,
+        ApiServiceProxies.HomeServiceProxy,
         ApiServiceProxies.RoleServiceProxy,
         ApiServiceProxies.SessionServiceProxy,
         ApiServiceProxies.TenantServiceProxy,
@@ -14,6 +14,7 @@ import * as ApiServiceProxies from './service-proxies';
         ApiServiceProxies.TokenAuthServiceProxy,
         ApiServiceProxies.AccountServiceProxy,
         ApiServiceProxies.NodeServiceProxy,
+        ApiServiceProxies.NodeDataServiceProxy,
         ApiServiceProxies.ConfigurationServiceProxy,
         { provide: HTTP_INTERCEPTORS, useClass: AbpHttpInterceptor, multi: true }
     ]
