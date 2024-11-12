f6486d27e6e7859fef475eecf0e37ba77107b4e3
Matimba Moses Hlongwani <Hlongwanimm@gmail.com>
2024-11-12 19:24:37 +0200

    Update AverageNodeData.cs

--- aspnet-core/src/Farmru.IotMonitoring.Core/Domains/Stats/AverageNodeData.cs
+++ aspnet-core/src/Farmru.IotMonitoring.Core/Domains/Stats/AverageNodeData.cs
@@ -9,13 +9,13 @@ namespace Farmru.IotMonitoring.Domains.Stats
 {
     public class AverageNodeData : Entity<Guid>
     {
-        public virtual double? AvgSoilTemperature { get; set; }
-        public virtual double? AvgSoilPH { get; set; }
-        public virtual double? AvgMoisture { get; set; }
-        public virtual double? AvgPhosphorus { get; set; }
-        public virtual double? AvgPotassium { get; set; }
-        public virtual double? AvgNitrogen { get; set; }
-        public virtual double? AvgSolarPanelVoltage { get; set; }
-        public virtual double? AvgBatteryVoltage { get; set; }
+        public virtual decimal? AvgSoilTemperature { get; set; }
+        public virtual decimal? AvgSoilPH { get; set; }
+        public virtual decimal? AvgMoisture { get; set; }
+        public virtual decimal? AvgPhosphorus { get; set; }
+        public virtual decimal? AvgPotassium { get; set; }
+        public virtual decimal? AvgNitrogen { get; set; }
+        public virtual decimal? AvgSolarPanelVoltage { get; set; }
+        public virtual decimal? AvgBatteryVoltage { get; set; }
     }
 }
